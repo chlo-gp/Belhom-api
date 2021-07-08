e12ca963d5e1b2010b930e71b885268f24a2e5d5
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-07-08 12:46:38 +0200

    fix:models files

--- articles/articles.model.js
+++ articles/articles.model.js
@@ -1,13 +1,21 @@
-module.exports = (sequelize, Sequelize) => {
+module.exports = (sequelize, Datatypes) => {
     const Articles = sequelize.define("articles", {
-        id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+        id: {type: Datatypes.INTEGER, autoIncrement: true, primaryKey: true},
+        title: {type: Datatypes.STRING, allowNull: false, },
+        category: {type: Datatypes.STRING, allowNull: false,},
+        lead: {type: Datatypes.STRING, },
+        content: {type: Datatypes.TEXT, allowNull: false,},
+        desc: {type: Datatypes.STRING, allowNull: false,},
+        thumbnail: {type: Datatypes.STRING },
+    });
+  
+    return Articles;
+  };
+
+  /* id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
         title: {type: Sequelize.STRING(255), allowNull: false, },
         category: {type: Sequelize.STRING(255), allowNull: false,},
         lead: {type: Sequelize.STRING(255), },
         content: {type: Sequelize.TEXT, allowNull: false,},
         desc: {type: Sequelize.STRING(255), allowNull: false,},
-        thumbnail: {type: Sequelize.STRING(255), },
-    });
-  
-    return Articles;
-  };
+        thumbnail: {type: Sequelize.STRING(255), }, */
--- products/products.model.js
+++ products/products.model.js
@@ -1,14 +1,23 @@
 module.exports = (sequelize, Sequelize) => {
     const Products = sequelize.define("products", {
         id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+        name: {type: Sequelize.STRING, allowNull: false,  },
+        brand: {type: Sequelize.STRING, allowNull: false,  },
+        category: {type: Sequelize.STRING, allowNull: false,},
+        price: {type: Sequelize.INTEGER, allowNull: false,},
+        desc: {type: Sequelize.STRING, },
+        apply: {type: Sequelize.STRING, },
+        thumbnail: {type: Sequelize.STRING,},
+    });
+  
+    return Products;
+  };
+
+  /*  id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
         name: {type: Sequelize.STRING(255), allowNull: false,  },
         brand: {type: Sequelize.STRING(255), allowNull: false,  },
         category: {type: Sequelize.STRING(255), allowNull: false,},
         price: {type: Sequelize.INTEGER, allowNull: false,},
         desc: {type: Sequelize.STRING(255), },
         apply: {type: Sequelize.STRING(255), },
-        thumbnail: {type: Sequelize.STRING(255),},
-    });
-  
-    return Products;
-  };
+        thumbnail: {type: Sequelize.STRING(255),}, */
